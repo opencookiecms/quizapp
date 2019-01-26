551af1b7c57448dcdc0aa785316fd77db480907b
opencookiecms <syed.m.afiq@outlook.com>
2019-01-27 03:04:27 +0800

    sd

--- quizapp/Properties/Settings.Designer.cs
+++ quizapp/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace quizapp.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\QuizData.accdb")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\\QuizData.accdb")]
         public string QuizDataConnectionString {
             get {
                 return ((string)(this["QuizDataConnectionString"]));
